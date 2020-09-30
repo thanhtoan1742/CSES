af9ea43d5be9a2898de16e3bf53959291c684a5e
thanhtoan1742 <thanhtoan1742@gmail.com>
2020-09-30 13:09:28 +0700

    add subarray sums 1 solution ajusted .gitignore so that it remove main executable

--- Sorting and Searching/Subarray Sums I/main.cpp
+++ Sorting and Searching/Subarray Sums I/main.cpp
@@ -5,10 +5,34 @@
 #define maximize(a, b) a = max((a), (b))
 
 using namespace std;
+const int maxN = 2e5 + 100;
+
+int n;
+long long x;
+int a[maxN];
+
+int res;
 
 int main(int argc, char* argv[]) {
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
 
-    
+    int n;
+    cin >> n >> x;
+    for (int i = 0; i < n; ++i) 
+        cin >> a[i];
+
+
+    res = 0;
+    long long s = 0;
+    int j = 0;
+    for (int i = 0; i < n; ++i) {
+        s += a[i];
+        while (j <= i && s > x)     
+            s -= a[j++];
+        
+        res += s == x;
+    }
+
+    cout << res << '\n';
 }
