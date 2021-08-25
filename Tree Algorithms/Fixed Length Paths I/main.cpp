091ed57e4e366ee76a08777c301b3037fff3d51b
thanhtoan1742 <thanhtoan1742@gmail.com>
2021-08-25 20:36:12 +0700

    fixed length path

--- Tree Algorithms/Fixed Length Paths I/main.cpp
+++ Tree Algorithms/Fixed Length Paths I/main.cpp
@@ -1,31 +1,28 @@
 #include <bits/stdc++.h>
- 
+
 using namespace std;
- 
+
 template <typename T>
 using vec = vector<T>;
 using ll = long long;
- 
+
 const int maxN = 2e5 + 100;
- 
+
 int n, k;
 vec<int> adj[maxN];
- 
+
 bool removed[maxN];
 int stsz[maxN];
- 
-struct {
-    int root;
-    int prev;
-    int cur;
-} f[maxN];
+
+int max_d;
+int f[maxN];
 ll res;
- 
+
 void read_input() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
- 
+
     cin >> n >> k;
     for (int i = 1; i < n; ++i) {
         int x, y;
@@ -34,84 +31,75 @@ void read_input() {
         adj[y].push_back(x);
     }
 }
- 
+
 void print_output() {
     cout << res << "\n";
 }
- 
+
 void cal_subtree_size(int u, int p = -1) {
     stsz[u] = 1;
     for (int v: adj[u]) {
         if (v == p || removed[v])
             continue;
- 
+
         cal_subtree_size(v, u);
         stsz[u] += stsz[v];
     }
 }
- 
+
 int find_centroid_dfs(int tsz, int u, int p = -1) {
     for (int v: adj[u]) {
         if (v == p || removed[v])
             continue;
- 
+
         if (stsz[v]*2 > tsz)
             return find_centroid_dfs(tsz, v, u);
     }
- 
+
     return u;
 }
- 
+
 int find_centroid(int u) {
     cal_subtree_size(u);
     return find_centroid_dfs(stsz[u], u);
 }
- 
-void count_path_dfs(int root, int u, int p = -1, int d = 0) {
+
+void count_path(int u, int p = -1, bool filling = true, int d = 0) {
     if (d > k)
         return;
- 
-    if (f[d].root != root) {
-        f[d].root = root;
-        f[d].cur = f[d].prev = 0;
-    }
-    if (f[k - d].root == root)
-        res += f[k - d].prev;
-    f[d].cur += 1;
-    if (u == root) {
-        f[0].cur = 0;
-        f[0].prev = 1;
-    }
- 
+
+    max_d = max(max_d, d);
+    if (filling)
+        f[d]++;
+    else
+        res += f[k - d];
+
     for (int v: adj[u]) {
-        if (v == p || removed[v])
+        if (removed[v] || v == p)
             continue;
- 
-        count_path_dfs(root, v, u, d + 1);
- 
-        if (u != root)
-            continue;
-        for (int i = 1; i <= k && f[i].root == root && f[i].cur != 0; ++i) {
-            f[i].prev += f[i].cur;
-            f[i].cur = 0;
-        }
+
+        count_path(v, u, filling, d + 1);
     }
 }
- 
-void count_path(int root) {
-    count_path_dfs(root, root);
-}
- 
+
 void centroid_decompose(int u = 1) {
     int root = find_centroid(u);
-    count_path(root);
- 
     removed[root] = true;
+
+    fill(f + 1, f + max_d + 1, 0);
+    f[0] = 1;
+    max_d = 0;
+    for (int v: adj[root])
+        if (!removed[v]) {
+            count_path(v, root, false, 1);
+            count_path(v, root, true, 1);
+        }
+
     for (int v: adj[root])
         if (!removed[v])
             centroid_decompose(v);
 }
- 
+
 int main() {
     read_input();
     centroid_decompose();
--- Tree Algorithms/Fixed Length Paths II/main.cpp
+++ Tree Algorithms/Fixed Length Paths II/main.cpp
@@ -1,164 +1,114 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-#include <ios>
- 
-#define ll long long
-#define ar array
-#define sqr(x) ((x) * (x))
-#define maximize(a, b) (a) = max((a), (b))
-#define minimize(a, b) (a) = min((a), (b))
-#define gb(a, n) (((a) >> (n))&1)
+#include <numeric>
+
+using namespace std;
+
+template <typename T>
+using vec = vector<T>;
+using ll = long long;
 
 const int maxN = 2e5 + 100;
- 
-using namespace std;
-using namespace __gnu_pbds;
-template<class T> using indexed_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
-struct block{
-    static int size;
-    int offset;
-    vector<ar<int, 2>> a;
+int n, k1, k2;
+vec<int> adj[maxN];
 
-    block(int len = block::size)
-    : offset(0), a(len) {
-        for (int i = 0; i < a.size(); ++i) {
-            a[i][0] = 0;
-            a[i][1] = i;
-        }
-    }
+bool removed[maxN];
+int stsz[maxN];
 
-    void update(int val, int l = 0, int r = size - 1) {
-        if (l == 0 && r == size - 1) {
-            offset += val;
-            return;
-        }
-
-        for (int i = 0; i < a.size(); ++i) {
-            a[i][0] += offset;
-            if (l <= a[i][1] && a[i][1] <= r)
-                a[i][0] += val;
-        }
-
-        offset = 0;
-        // sort(a.begin(), a.end());
-    }
-
-    int get(int val, int l = 0, int r = size - 1) {
-        if (l == 0 && r == size - 1) {
-            ar<int, 2> t = {val - offset, maxN};
-            return upper_bound(a.begin(), a.end(), t) - a.begin();
-            return 0;
-        }
-
-        update(0);
-        int res = 0;
-        for (int i = 0; i < a.size(); ++i)
-            if (l <= a[i][1] && a[i][1] <= r && a[i][0] <= val)
-                ++res;
-        return res;
-    }
-};
-int block::size = 0;
-
-int n;
-int k1, k2;
-vector<int> adj[maxN];
-
-int pos = 1;
-int b[maxN], e[maxN];
-
-vector<block> blk;
+int max_d;
+int cnt[maxN], scnt[maxN];
 ll res;
- 
-void add(int l, int r, int val) {
-    l--; r--;
-    // cerr << "add: " << l << " " << r << " " << val << "\n";
-    int bl = l/block::size;
-    int br = r/block::size;
-    l %= block::size;
-    r %= block::size;
 
-    if (bl == br) {
-        blk[bl].update(val, l, r);
-        return;
-    }
-
-    blk[bl].update(val, l, block::size - 1);
-    blk[br].update(val, 0, r);
-    for (int i = bl + 1; i < br; ++i)
-        blk[i].update(val);
-}
-
-int get(int l, int r, int val) {
-    l--; r--;
-    // cerr << "get: " << l << " " << r << " " << val << "\n";
-    int bl = l/block::size;
-    int br = r/block::size;
-    l %= block::size;
-    r %= block::size;
-
-    if (bl == br) {
-        return blk[bl].get(val, l, r);
-    }
-
-    int s = 0;
-    s += blk[bl].get(val, l, block::size - 1);
-    s += blk[br].get(val, 0, r);
-    for (int i = bl + 1; i < br; ++i)
-        s += blk[i].get(val);
-
-    return s;
-}
-
-void DFS(int u = 1, int p = 0) {
-    // cerr << u << "\n";
-    b[u] = e[u] = pos++;
-
-    if (b[u] > 1) {
-        add(1, b[u] - 1, 1);
-        res -= get(1, b[u] - 1, k1);
-        res += get(1, b[u] - 1, k2);
-    }
-
-    for (int v: adj[u]) {
-        if (v == p)
-            continue;
-
-        DFS(v, u);
-        e[u] = e[v];
-    }
-
-    if (b[u] > 1) {
-        add(b[u], e[u], 1);
-        add(1, b[u] - 1, -1);
-    }
-}
-
-
-int main() {
-    // freopen("input.txt", "r", stdin);
+void read_input() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
 
     cin >> n >> k1 >> k2;
-    k1--;
     for (int i = 1; i < n; ++i) {
-        int u, v;
-        cin >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
+        int x, y;
+        cin >> x >> y;
+        adj[x].push_back(y);
+        adj[y].push_back(x);
     }
+}
 
-    block::size = sqrt(n);
-    for (int i = 0, sz = n; sz; ++i) {
-        int len = min(sz, block::size);
-        sz -= len;
-        blk.emplace_back(len);
-    }
-    DFS();
-
+void print_output() {
     cout << res << "\n";
+}
+
+void cal_subtree_size(int u, int p = -1) {
+    stsz[u] = 1;
+    for (int v: adj[u]) {
+        if (v == p || removed[v])
+            continue;
+
+        cal_subtree_size(v, u);
+        stsz[u] += stsz[v];
+    }
+}
+
+int find_centroid_dfs(int tsz, int u, int p = -1) {
+    for (int v: adj[u]) {
+        if (v == p || removed[v])
+            continue;
+
+        if (stsz[v]*2 > tsz)
+            return find_centroid_dfs(tsz, v, u);
+    }
+
+    return u;
+}
+
+int find_centroid(int u) {
+    cal_subtree_size(u);
+    return find_centroid_dfs(stsz[u], u);
+}
+
+void count_path(int u, int p = -1, bool filling = true, int d = 0) {
+    if (d > k2)
+        return;
+
+    max_d = max(max_d, d);
+    if (filling)
+        cnt[d]++;
+    else {
+        int i = max(0, k1 - d);
+        int j = (k2 - d);
+        res += scnt[i] - scnt[j + 1];
+        // res += accumulate(f + i, f + j + 1, 0);
+    }
+
+    for (int v: adj[u]) {
+        if (removed[v] || v == p)
+            continue;
+
+        count_path(v, u, filling, d + 1);
+    }
+    scnt[d] = scnt[d + 1] + cnt[d];
+}
+
+void centroid_decompose(int u = 1) {
+    int root = find_centroid(u);
+    removed[root] = true;
+
+	fill(cnt + 1, cnt + max_d + 1, 0); cnt[0] = 1;
+    fill(scnt + 1, scnt + max_d + 1, 0); scnt[0] = 1;
+    max_d = 0;
+    for (int v: adj[root])
+        if (!removed[v]) {
+            count_path(v, root, false, 1);
+            count_path(v, root, true, 1);
+            scnt[0] = scnt[1] + cnt[0];
+        }
+
+    for (int v: adj[root])
+        if (!removed[v])
+            centroid_decompose(v);
+}
+
+int main() {
+    read_input();
+    centroid_decompose();
+    print_output();
 }
