7a566258c6c43e3520ec292e084ca40f81377faa
Thanh Toan <30596211+thanhtoan1742@users.noreply.github.com>
2024-04-16 23:41:41 +0700

    Update main.cpp
    Download speed

--- Graph Algorithms/Download Speed/main.cpp
+++ Graph Algorithms/Download Speed/main.cpp
@@ -1,89 +1,111 @@
 #include <bits/stdc++.h>
- 
-using namespace std;
-const int maxN = 500 + 100;
-const int maxM = 1000 + 100;
-const long long maxFlow = 1e18;
- 
-#define sqr(x) ((x) * (x))
-#define maximize(a, b) (a) = max((a), (b))
-#define minimize(a, b) (a) = min((a), (b))
- 
-struct Edge {
-    int u, v, irev;
-    long long c;
-};
- 
-int n, m;
-Edge e[maxM*2];
-vector<int> adj[maxN];
- 
-int lev[maxN];
- 
-vector<int> path;
- 
-bool find_flow() {
-    fill(lev + 1, lev + 1 + n, 0);
-    queue<int> q;
-    lev[1] = 1;
-    q.push(1);
- 
-    while (q.size()) {
-        int u = q.front();
-        q.pop();
- 
-        if (u == n) 
-            return 1;
- 
-        for (int i: adj[u])
-            if (e[i].c && !lev[e[i].v]) {
-                lev[e[i].v] = lev[u] + 1;
-                q.push(e[i].v);
-            }
-    }
- 
-    return 0; 
-}
- 
-long long add_flow(int u = 1, long long inflow = maxFlow) {
-    if (u == n) 
-        return inflow;
-    if (inflow == 0)
-        return 0;
 
-    long long og_inflow = inflow;
- 
-    for (int i: adj[u])
-        if (lev[e[i].v] == lev[u] + 1) {
-            long long t = add_flow(e[i].v, min(inflow, e[i].c));
-            e[i].c -= t;
-            e[e[i].irev].c += t;
-            inflow -= t;
-        }
- 
-    return og_inflow - inflow;
+#define LP(u) (#u) << ": " << (u) << " "
+
+using namespace std;
+
+using ll = long long;
+
+const int maxN = 510;
+const int maxM = 1010;
+const ll maxF = 2e15 + 10;
+
+struct Edge {
+  int u;
+  int v;
+  ll c;
+  ll uf;
+};
+
+int n, m;
+Edge e[maxM];
+vector<int> eo[maxN];
+
+int pre[maxN];
+ll flow[maxN];
+
+
+ll find_flow() {
+  fill(pre, pre + n + 1, 0);
+  fill(flow, flow + n + 1, 0);
+
+  queue<int> q;
+  q.push(1);
+  flow[1] = maxF;
+  pre[1] = -1;
+
+  
+  while (!q.empty()) {
+    int u = q.front();
+    q.pop();
+
+    // cout << LP(u) << "\n";
+
+    if (u == n)
+      break;
+
+    for (int i: eo[u]) {
+      auto [x, y, c, uf] = e[abs(i)];
+      int v = (i > 0) ? y : x;
+      if (pre[v] != 0)
+        continue;
+
+      if (i > 0) {
+        if (uf == c)
+          continue;
+        flow[v] = min(flow[u], c - uf);
+      } else {
+        if (uf == 0)
+          continue;
+        flow[v] = min(flow[u], uf);
+      }
+
+      pre[v] = i;
+      q.push(v);
+      // cout << LP(i) << LP(v) << LP(flow[u]) << LP(c) << LP(uf) << LP(flow[v]) << "\n";
+    }
+
+  }
+
+
+  ll res = flow[n];
+  if (res == 0)
+    return 0;
+
+
+  int v = n;
+  while (v != 1) {
+    int i = pre[v];
+    if (i > 0) {
+      e[i].uf += res;
+      v = e[i].u;
+    } else {
+      e[-i].uf -= res;
+      v = e[-i].v;
+    }
+  }
+
+  return res;
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
- 
-    cin >> n >> m;
-    for (int i = 0; i < m; ++i) {
-        int x, y;
-        long long c;
-        cin >> x >> y >> c;
-        
-        e[i] = {x, y, i + m, c};
-        e[i + m] = {y, x, i, 0};
-        adj[x].push_back(i);
-        adj[y].push_back(i + m);
-    }
- 
-    long long flow = 0;
-    while (find_flow())
-        flow += add_flow();
-    
-    cout << flow << '\n';
+  cin >> n >> m;
+  for (int i = 1; i <= m; ++i) {
+    cin >> e[i].u >> e[i].v >> e[i].c;
+    e[i].uf = 0;
+    eo[e[i].u].push_back(i);
+    eo[e[i].v].push_back(-i);
+  }
+
+  ll res = 0;
+  while (true) {
+    ll a = find_flow();
+    // cout << LP(a) << "\n";
+    // for (int i = 1; i <= m; ++i) cout  << LP(e[i].u) << LP(e[i].v) << LP(e[i].uf) << LP(e[i].c) << "\n";
+    if (a == 0)
+      break;
+    res += a;
+  }
+
+  cout << res << "\n";
 }
